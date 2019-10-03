ad63126d5332fccd723c630606b65af180af7761
Aash90 <ash8690@gmail.com>
2019-10-03 21:00:06 +0530

    Update game_objects.py

--- game_objects.py
+++ game_objects.py
@@ -1,5 +1,20 @@
-from enum import Enum
+"""
+This file is responsible creating/removing Objects and their associated Rules for the game.
 
+For e.g
+
+"Rock":{
+                "beats_to" : ["Scissor", "Lizard"],
+                "lose_to"  : ["Paper", "Spock"]
+        }
+In the above example the rule is defined for the "Rock" object.
+
+The "beats_to" value is a list of Objects which are defeated by Rock(i.e lose againts Rock)
+The "lose_to" value is a list of Objects which defeat the Rock(i.e beats Rock)
+So in order to extend the game for more and more objects. User is required to add these rules
+and an entry in the GAME_RULES object as shown in above sample.
+
+"""
 
 GAME_RULES = {
              "Rock":{
